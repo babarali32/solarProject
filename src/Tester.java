993c9525c6e9c03c82a37d098c1e1b1473046420
BabarAli <babaraliengineer32@gmail.com>
2023-12-14 21:20:19 +0500

    basic structure is deployed

--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,10 @@
 public class Tester {
     public static void main(String[] args) {
         solarSystem milkyway=new solarSystem();
-
+        milkyway.sun="1";
+        milkyway.moon="2";
+        milkyway.earth="3";
+        
 
     }
 }
